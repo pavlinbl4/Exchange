2cb479308cb2ebe136d40ee3ce1d252b98b08a4e
pavlinbl4@gmail.com <15deaDman>
2020-06-27 16:37:21 +0300

    список уменньшается и дохожит по пустого, но мне кажется это не правильный путь

--- 2.5 Рекурсия.Перебор подмножеств..py
+++ 2.5 Рекурсия.Перебор подмножеств..py
@@ -1,13 +1,27 @@
-def podm(mn):
+def podm(mn,n):
+    if n == 0:
+
+        print("END")
+    else:
+        print(mn)
+        mn.pop(n-1)
+        podm(mn,n-1)
+
+
+
+
+
     
 
 
 
 
 
-mn = input().split(" ") # создаю список из вводимых данных
-mn.sort()
+# mn = input().split(" ") # создаю список из вводимых данных
+# mn.sort()
+mn = [ 1,2,3,4]
 n = len(mn)
-print(mn)
+#print(mn)
+podm(mn,n)
 
 
