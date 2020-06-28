2c57f40ceaa3afcc97d8aba2af761023766b5106
pavlinbl4@gmail.com <15deaDman>
2020-06-28 13:06:44 +0300

    также как в предыдущей задаче, реализую ввод до точки, но здесь разделитель не пробел а тире

--- Ввод текста до точки.py
+++ Ввод текста до точки.py
@@ -1,11 +1,6 @@
-def text_input():
-    text = []
-    while True:
-        s = input()
-        if s == '.':
-            break
-        else:
-            text.append(s)
-    return text
-
-print(text_input())
+while True:
+    s = input()
+    if s == '.':
+        break
+    else:
+        text.append(s)
